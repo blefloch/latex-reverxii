e41272949e681948b1412b17fde2813de1fcde72
Bruno Le Floch <blflatex@gmail.com>
2015-09-20 08:00:29 +0200

    Include reverxii.tex in the set of source files
    Otherwise it did not get into the CTAN zip, nor into the pdf

--- build.lua
+++ build.lua
@@ -1,6 +1,7 @@
 #!/usr/bin/env texlua
 -- Build script for reverxii
 module = "reverxii"
+sourcefiles = {"reverxii.dtx", "reverxii.tex"}
 
 kpse.set_program_name("kpsewhich")
 dofile(kpse.lookup("l3build.lua"))
